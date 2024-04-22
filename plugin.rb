01a17e7bd90c3d865b8d8e45910d57f6c5ef9076
scavin <scavina@gmail.com>
2024-04-22 10:39:22 +0800

    Update plugin.rb
    ## Adapt to new domain
    
    * www.bilibili.com
    * m.bilibili.com
    
    ## match / symbol.

--- plugin.rb
+++ plugin.rb
@@ -11,11 +11,11 @@ after_initialize do
       class BilibiliOnebox
         include Onebox::Engine
 
-        REGEX = /^https?:\/\/www\.bilibili\.com\/video\/(.*?)(?=\/|$)/
+        REGEX = /^https?:\/\/(www|m)\.bilibili\.com\/video\/(.*?)(?=\/|\?|$)/
         matches_regexp REGEX
 
         def to_html
-          video_id = @url.match(REGEX)[1]
+          video_id = @url.match(REGEX)[2]
           "<iframe src='https://player.bilibili.com/player.html?bvid=#{video_id}&high_quality=1&autoplay=0' scrolling='no' border='0' frameborder='no' width='100%' height='430' allowfullscreen='true'></iframe>"
         end
       end
